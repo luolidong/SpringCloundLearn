f16084ca27db4e21d9f44ee22c007fc5262b63ab
luolidong <luolidong@shzhanmeng.com>
2019-03-12 20:42:56 +0800

    add 7

--- config/src/main/java/com/imooc/ConfigApplication.java
+++ config/src/main/java/com/imooc/ConfigApplication.java
@@ -3,15 +3,15 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class OrderApplication {
+@EnableConfigServer
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 
 }
